faa48c7cd33ae26fcf9b95b3f56f181966120f6f
Amrinder Singh <51396118+amrinderRahal@users.noreply.github.com>
2023-11-26 21:35:22 +0530

    Initial Commit

---
+++ SampleApp/SampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleAppApp.swift
+//  SampleApp
+//
+//  Created by Amrinder on 26/11/23.
+//
+
+import SwiftUI
+
+@main
+struct SampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
